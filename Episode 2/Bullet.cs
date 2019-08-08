c5dbef69d5c14cf7d3bd7983ff36fbbad734a708
Ubalube <35713847+Ubalube@users.noreply.github.com>
2019-08-08 14:16:11 -0700

    Update Bullet.cs

--- Episode 2/Bullet.cs
+++ Episode 2/Bullet.cs
@@ -4,31 +4,45 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-
+    
+    //Our Damage Variable
     public float Damage;
-
+    
+    //When the bullet collides with something
     private void OnCollisionEnter(Collision collision)
     {
+        //Send a message to the Log saying Bullet hit object
         Debug.Log("Bullet hit object");
+        //Destroy the bullet object
         Destroy(this.gameObject);
-
+    
+        
+        //Get the Living Script of the object 
         Living l = collision.gameObject.GetComponent<Living>();
+        //If the object has a Living script
         if(l != null)
         {
+            //Then do Damage
             l.Damage(Damage);
         }
 
     }
-
+    
+    //When the script starts
     private void Start()
     {
+        //Start the startLife coroutine
         StartCoroutine(startLife());
     }
-
+    
+    //Our startLife IEnumerator
     public IEnumerator startLife()
     {
+        //Wait for 2 Seconds
         yield return new WaitForSeconds(2);
+        //Send a message to the log saying Bullet reached its range
         Debug.Log("Bullet reached its range");
+        //Destroy the bullet
         Destroy(this.gameObject);
     }
 }
